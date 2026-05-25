1ee1e07af843294b1b604b9df584bcb962a05b4d
sohaibabuserour-cyber <sohaibabuserour@gmail.com>
2026-05-25 15:04:19 +0300

    Delete ui.js

--- ui.js
+++
@@ -1,5 +0,0 @@
-export function initUI(){
- document.getElementById('btnMap').onclick=()=>alert('Map View');
- document.getElementById('btnReport').onclick=()=>alert('Reports');
- console.log('ui ready');
-}
